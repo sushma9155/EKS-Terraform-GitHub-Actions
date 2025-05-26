d98d4643a7a9a1f977af23e23ad0153e2eec30bf
sushma9155 <sushmadhadde@gmail.com>
2025-05-27 00:32:31 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,8 +7,8 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "dev-aman-tf-bucket"
-    region         = "us-east-1"
+    bucket         = "dev-sushma-tf-bucket"
+    region         = "ap-south-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
     encrypt        = true
